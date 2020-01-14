175cc12f7d2c42a1705cb4df22faa62f76583cf6
Innocent nnadili <innocent.n@buffer.media>
2020-01-14 05:07:25 -0800

    perform the following implementation
    - Install the npm package laravel-mix-imagemin
    - create the following files in the frontend directory  main, footer,nav
    - create the following file welcome.blade.php in the view directory

---
+++ resources/views/layouts/frontend/footer.blade.php
@@ -0,0 +1,48 @@
+<div class="row">
+    <div class="col-6 col-md-3">
+        <div>
+            <a href="" alt=""><img src="" alt=""></a>
+            <p>Buffer is a digital media company and mobile value added service provider focused on delivering innovative mobile solutions to the global media market.</p>
+        </div>
+    </div>
+    <div class="col-6 col-md-3">
+        <header>
+            <h3>ABOUT US</h3>
+        </header>
+        <div>
+            <ul class="list-unstyled">
+                <li><a href="">Company</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Careers</a></li>
+                <li><a href="">Blog</a></li>
+            </ul>
+        </div>   
+    </div>
+    <div class="col-6 col-md-3">
+        <header>
+            <h3>SOLUTIONS</h3>
+        </header>
+        <div>
+            <ul class="list-unstyled">
+                <li><a href="">Enterprise Messaging</a></li>
+                <li><a href="">Voice</a></li>
+                <li><a href="">Mobile Vas</a></li>
+                <li><a href="">Digital Marketing</a></li>
+                <li><a href="">Content Distribution</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="col-6 col-md-3">
+        <header>
+            <h3>MEDIA</h3>
+        </header>
+        <div>
+            <ul class="list-unstyled">
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </div> 
+    </div>
+</div>
