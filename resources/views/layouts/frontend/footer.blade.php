1d808ea6ab5db31616dcf64e5d1d7abfb1515dde
Innocent nnadili <innocent.n@buffer.media>
2020-01-14 23:42:34 -0800

    perform the following ops on the frontend footer
    - changed the text color
    - add maggin on all the h5 tags

--- resources/views/layouts/frontend/footer.blade.php
+++ resources/views/layouts/frontend/footer.blade.php
@@ -1,9 +1,9 @@
 <div class="px-2 px-md-0">
-    <div class="row text-white mx-0 ">
+    <div class="row  footer-text-color mx-0 ">
         <div class="col-12 col-md-3 mb-5 mb-md-0">
             <header>
-                <h3 class="mt-3 mt-md-0">
-                <a href="" alt=""><img src="images/logo/footer_logo.png" alt=""></a>
+                <h3 class="mt-3 mt-md-0 mb-3">
+                    <a href="" alt=""><img src="images/logo/footer_logo.png" alt="buffer media logo" style="width:50%"></a>
                 </h3>
             </header>
             <div>
@@ -17,8 +17,8 @@
                         <div class="input-group">
                             <input type="email" class="form-control" name="email" placeholder="example@example.com" id="email">
                             <div class="input-group-append">
-                                <button class="btn comp-background" type="submit">
-                                    <i class="fa fa-paper-plane text-white"></i>
+                                <button class="btn comp-background text-white" type="submit">
+                                    <i class="fa fa-paper-plane "></i>
                                 </button>
                             
                             </div>
@@ -30,10 +30,10 @@
         </div>
         <div class="col-12 col-md-3 mb-5 mb-md-0">
             <header>
-                <h4 class="mt-3 mt-md-0">ABOUT US</h4>
+                <h5 class="mt-3 mt-md-0 mb-4 text-white">About Us</h5>
             </header>
             <div>
-                <ul class="list-unstyled">
+                <ul class="list-unstyled footer-list footer-anchor">
                     <li><a href="">Company</a></li>
                     <li><a href="">Contact</a></li>
                     <li><a href="">Careers</a></li>
@@ -43,10 +43,10 @@
         </div>
         <div class="col-12 col-md-3 mb-5 mb-md-0">
             <header>
-                <h4 class="mt-3 mt-md-0">SOLUTIONS</h4>
+                <h5 class="mt-3 mt-md-0 mb-4 text-white">Solutions</h5>
             </header>
             <div>
-                <ul class="list-unstyled">
+                <ul class="list-unstyled footer-list footer-anchor">
                     <li><a href="">Enterprise Messaging</a></li>
                     <li><a href="">Voice</a></li>
                     <li><a href="">Mobile Vas</a></li>
@@ -57,19 +57,24 @@
         </div>
         <div class="col-12 col-md-3 mb-5 mb-md-0">
             <header>
-                <h4 class="mt-3 mt-md-0">MEDIA</h4>
+                <h5 class="mt-3 mt-md-0 mb-4 text-white">Media Contact</h5>
             </header>
             <div>
-                <ul class="list-inline">
-                    <li class="list-inline-item"><a href="https://www.facebook.com/buffermediaLTD/" title="Buffer Facebook Link"><i class="b-text-color fa fa-facebook" aria-hidden="true"></i></a></li>
-                    <li class="list-inline-item"><a href="https://twitter.com/BufferMedia" title="Buffer Twitter Link"><i class="b-text-color fa fa-twitter" aria-hidden="true"></i></a></li>
-                    <li class="list-inline-item"><a href="https://pinterest.com/buffermedia" title="Buffer Pinterest Link"><i class="b-text-color fa fa-pinterest" aria-hidden="true"></i></a></li>
-                    <li class="list-inline-item"><a href="https://www.linkedin.com/company/buffer-media-limited/" title="Buffer Linked In Link"><i class="b-text-color fa fa-linkedin" aria-hidden="true"></i></a></li>
+                <ul class="list-inline footer-anchor footer-anchor-media">
+                    <li class="list-inline-item"><a href="https://www.facebook.com/buffermediaLTD/" class="rounded-circle p-2" title="Buffer Facebook Link"><i class="b-text-color fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li class="list-inline-item"><a href="https://twitter.com/BufferMedia" class="rounded-circle p-2" title="Buffer Twitter Link"><i class="b-text-color fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li class="list-inline-item"><a href="https://pinterest.com/buffermedia" class="rounded-circle p-2" title="Buffer Pinterest Link"><i class="b-text-color fa fa-pinterest" aria-hidden="true"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.linkedin.com/company/buffer-media-limited/" class="rounded-circle p-2" title="Buffer Linked In Link"><i class="b-text-color fa fa-linkedin" aria-hidden="true"></i></a></li>
+                </ul>
+                <ul class="list-unstyled footer-list footer-anchor">
+                    <li><a href="">Suport</a></li>
+                    <li><a href="">Sales</a></li>
+                    <li><a href="">Developer</a></li>
                 </ul>
             </div> 
         </div>
     </div>
 </div>
-<div class="text-center pt-2 text-white">
+<div class="text-center pt-2 footer-text-color">
     <small>{{date('Y')}} &copy; Buffer Media</small>
 </div>
