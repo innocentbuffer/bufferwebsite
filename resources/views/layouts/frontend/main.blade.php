a8279793285f2793a256f3e8f0f5e88625686b96
Innocent nnadili <innocent.n@buffer.media>
2020-01-15 02:04:20 -0800

    insert the favicon image and make the navbar sticky

--- resources/views/layouts/frontend/main.blade.php
+++ resources/views/layouts/frontend/main.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+
+    <link rel="icon" type="image/png" href="images/logo/favicon.ico">
+
     <!-- CDN compiled and minified stylesheet -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -15,7 +18,7 @@
 </head>
 <body>
     <!-- header start -->
-    <header>
+    <header class="buffer-navbar">
         @include('layouts.frontend.nav')
     </header>
     <!-- header end -->
