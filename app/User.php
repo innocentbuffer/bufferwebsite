699bde83f1e2135718fb24ecd51ad97701fd7eb8
Innocent Nnadili <innoveran@gmail.com>
2020-02-08 18:47:34 +0100

    implement the schema for role and role_user migration

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function roles()
+    {
+        return $this->belongToMany('App\Role')->withTimestamps();
+    }
 }
